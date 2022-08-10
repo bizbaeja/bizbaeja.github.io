eface92d9e3f4a6a58209fac8325318fc99d4811
bizbaeja <75407823+bizbaeja@users.noreply.github.com>
2022-08-10 19:55:49 +0900

    481

--- src/pages/NewBlog.js
+++ src/pages/NewBlog.js
@@ -1,10 +1,28 @@
+import { useHistory } from 'react-router-dom';
+
 import NewBlogForm from '../components/blogs/NewBlogForm';
 
 function NewBlogPage() {
+  const history= useHistory();
+
+  function addBlogHandler(blogData){
+    fetch(
+
+      'https://react-blog-53c3c-default-rtdb.firebaseio.com/blogs.json',{
+        mtehod:'POST',
+        body: JSON.stringify(blogData),
+        headers:{
+          'Content-Type': 'applycation.json'
+        }
+      }
+    ).then(()=>{
+      history.replace('/')
+    });
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewBlogForm />
+      <NewBlogForm onAddBlog={addBlogHandler}/>
     </section>
   );
 }
